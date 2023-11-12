0590295e298f9d6f8bc6ae2330dd739033ae1cdb
Vishnu Vardhan Vankayalapati <vishnuvankayalapati@giggi.io>
2023-11-11 19:26:15 -0500

    Area of rectangle

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@
 
 public class helloworld {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+        System.out.print("Hello World! ");
     }
 }
---
+++ rectanglearea.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class rectanglearea {
+    public static void main(String[] args) {
+        Scanner ob = new Scanner(System.in);
+        double length = ob.nextDouble();
+        double breadth = ob.nextDouble();
+        System.err.println("Area of Rectangle: " + length*breadth);
+        ob.close();
+    }
+}
