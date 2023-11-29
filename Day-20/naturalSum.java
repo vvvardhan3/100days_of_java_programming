573cd98940e32833c7904439f231d25381a037dd
Vishnu Vardhan Vankayalapati <vishnuvankayalapati@giggi.io>
2023-11-29 11:21:47 -0500

    Updated Sum of N natural numbers

--- Day-20/naturalSum.java
+++ Day-20/naturalSum.java
@@ -7,12 +7,12 @@ public class naturalSum {
         Scanner obj = new Scanner(System.in);
         
         System.out.println("Enter a number: ");
-        int input = obj.nextInt();
+        int input = obj.nextInt();  // input :
 
-        //int sum = (input/2) * (input + 1);
-        int sum = 0;
-        for(int i = 0; i <= input;i++){
-            sum = sum + i;
+        //int sum = (input/2) * (input + 1); // Formula for finding the sum of 'N' Natural numbers
+        int sum = 0;  // sum = 0;
+        for(int i = 0; i <= input;i++){  // i--> 1,2,3,4,5,6
+            sum = sum + i;  // 1+2+3+4+5+6 --> 21
         }
 
 
