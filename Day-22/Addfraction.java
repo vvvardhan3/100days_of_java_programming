26c7ae0de7b49193396de6ad5faed34fd96dfe48
Vishnu Vardhan Vankayalapati <vishnuvankayalapati@giggi.io>
2023-11-29 15:53:15 -0500

    Adding two Fractions

--- Day-22/Addfraction.java
+++ Day-22/Addfraction.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class fraction {
+public class Addfraction {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         System.out.println("Enter value of x1: ");
