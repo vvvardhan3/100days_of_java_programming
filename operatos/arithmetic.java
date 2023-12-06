2adfaaba373b4fdef55193b474dccf8e93f7a96f
Vishnu Vardhan Vankayalapati <vishnuvankayalapati@giggi.io>
2023-12-06 02:13:20 -0500

    Arithmetic

--- operatos/arithmetic.java
+++ operatos/arithmetic.java
@@ -1,5 +1,23 @@
 package operatos;
 
 public class arithmetic {
-    
+    public static void main(String[] args) {
+        int i = 20;
+        int j = 15;
+        int k = 10;
+        int p = 100;
+        int v = 50;
+
+        i += 5; // Addition
+        j -= 5; // Substraction
+        k *= 5; // Multiplication
+        p /= 10; // Division
+        v %= 2; // Modulus
+        
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(p);
+        System.out.println(v);
+    }
 }
