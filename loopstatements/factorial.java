77cf1038959b241f239b42cdb7f5b929c82f2bbc
Vishnu Vardhan Vankayalapati <vishnuvankayalapati@giggi.io>
2023-12-08 14:03:21 -0500

    Factorial Java

---
+++ loopstatements/factorial.java
@@ -0,0 +1,20 @@
+package loopstatements;
+
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int input = obj.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= input; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+
+        obj.close();
+    }
+    
+}
