8a2f9a7dbeb5cf30009ed5a470f5c2d201bfcbf1
Vishnu Vardhan Vankayalapati <vishnuvankayalapati@giggi.io>
2023-12-10 22:46:43 -0500

    For-Loop

---
+++ loopstatements/forloop.java
@@ -0,0 +1,20 @@
+// A different way to write a java for-loop 
+package loopstatements;
+
+// import java.util.Scanner;
+
+public class forloop {
+    public static void main(String[] args) {
+        // Scanner obj = new Scanner(System.in);
+        for(int i = 1; ;){
+            
+            if (i<=10) {
+                System.out.println(i);
+                i++;
+            }
+            else
+            break;
+        } 
+    }
+    
+}
