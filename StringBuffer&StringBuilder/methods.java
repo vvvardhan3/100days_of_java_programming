8ba1377ec5c313bc26ce7a2020d82662fa5e2be5
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-01-21 13:40:37 -0500

    methods

--- StringBuffer&StringBuilder/methods.java
+++ StringBuffer&StringBuilder/methods.java
@@ -1,17 +1,17 @@
 public class methods {
     public static void stringbuffer(){
         // String vishnu = "VishnuVardhan";
-        StringBuffer sb = new StringBuffer("Katta");
-        StringBuffer sb1 = new StringBuffer("Katta");
-        //StringBuffer sb2 = new StringBuffer(vishnu);
+        StringBuffer sb = new StringBuffer("Vishnu");
+        StringBuffer sb1 = new StringBuffer("Vardhan");
+        // StringBuffer sb2 = new StringBuffer(vishnu);
         // sb.append("Preethi");
         // sb1.insert(0, "Vishnu");
         // sb1.deleteCharAt(0);
-        //sb2.reverse();
+        // sb2.reverse();
         // System.out.println(sb.codePointAt(3)); // Returns ACSII value at a given position
-        //System.out.println(sb.compareTo(sb1));
-        sb.replace(0, 4, "Vishnu");
-        System.out.println(sb);
+        // System.out.println(sb.compareTo(sb1));
+        // sb.replace(0, 4, "Vishnu");
+        //    System.out.println(sb);
         
         // System.out.println(sb.capacity()); // 16(Default Size) + 5 Characters
         
