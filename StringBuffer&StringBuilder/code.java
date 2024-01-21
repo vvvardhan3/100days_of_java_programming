a4ffd3b93c1e81361cacc0ef1fe0538d06d94527
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-01-21 11:00:31 -0500

    StringBuffer and StringBuilder

---
+++ StringBuffer&StringBuilder/code.java
@@ -0,0 +1,12 @@
+
+public class code {
+    public static void string(){
+        String s1 = "Vishnu";
+        String s2 = "Vishnu";
+        System.out.println(s1 == s2);
+    }
+    public static void main(String[] args) {
+        string();
+    }
+    
+}
